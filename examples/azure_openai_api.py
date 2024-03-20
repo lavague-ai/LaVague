b04cefbed119fe06959a5be7941122405b49872a
Daniel Huynh <daniel.huynh@mithrilsecurity.io>
2024-03-20 14:49:29 +0000

    Improved examples

--- examples/azure_openai_api.py
+++ examples/azure_openai_api.py
@@ -1,18 +1,31 @@
 from lavague import ActionEngine, CommandCenter
-from lavague.defaults import AzureOpenAILLM, DefaultEmbedder
-from lavague.prompts import DEFAULT_PROMPT
+from lavague.defaults import DefaultEmbedder
 import os.path
 
 homedir = os.path.expanduser("~")
 
-# To use AzureOpenAILLM you need to deploy your own gpt model on the azure-openai platform
-# Then you should either pass or define AZURE_OPENAI_TOKEN, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_DEPLOYMENT_NAME
+import os
+from llama_index.llms.azure_openai import AzureOpenAI
+
+api_key=os.getenv("AZURE_OPENAI_KEY")
+api_version="2023-05-15"
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+model = "gpt-4"
+deployment_name = "gpt-4-turbo"
+
+llm = AzureOpenAI(
+    model=model,
+    deployment_name=deployment_name,
+    api_key=api_key,
+    azure_endpoint=azure_endpoint,
+    api_version=api_version,
+    temperature=0.0
+)
 
 commandCenter = CommandCenter(
     ActionEngine(
-        AzureOpenAILLM(model="gpt-4", api_version="2023-05-15"),
+        llm,
         DefaultEmbedder(),
-        DEFAULT_PROMPT,
     ),
     chromePath=f"{homedir}/chrome-linux64/chrome",
     chromedriverPath=f"{homedir}/chromedriver-linux64/chromedriver",
