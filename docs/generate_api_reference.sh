351c16eab25e4b68dd3232a2e9b6ade64f129b72
lyie28 <laurayie93@gmail.com>
2024-03-20 16:23:35 +0100

    update requirements

--- docs/generate_api_reference.sh
+++ docs/generate_api_reference.sh
@@ -2,7 +2,7 @@
 set -e  # Exit if any command fails
 
 # Install pdoc3 for generating Python API documentation
-pip install pdoc3
+pip install lavague gradio pdoc3
 
 # Generate HTML documentation
 pdoc --html --skip-errors --template-dir docs/pdoc_template -o docs/docs/ src/lavague/ --force
