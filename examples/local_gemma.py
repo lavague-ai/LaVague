48bad7b6841623bdda020a55a316111b389443d3
Daniel Huynh <daniel.huynh@mithrilsecurity.io>
2024-03-20 13:15:15 +0000

    Corrected Gemma example

--- examples/local_gemma.py
+++ examples/local_gemma.py
@@ -1,32 +1,47 @@
 from lavague import CommandCenter, ActionEngine
-from lavague.defaults import DefaultLocalLLM, DefaultEmbedder
 from lavague.prompts import GEMMA_PROMPT
 from transformers import BitsAndBytesConfig
 from torch import bfloat16
 import os.path
+from llama_index.llms.huggingface import HuggingFaceLLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+from lavague.defaults import DefaultEmbedder
 
 homedir = os.path.expanduser("~")
 
 # Beware, if you don't have a nvidia GPU, this quantization config won't work
 # To check if you have one configured correctly, you can run python -m bitsandbytes in your terminal
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=bfloat16,
-)
+model_id = "HuggingFaceH4/zephyr-7b-gemma-v0.1"
 
+quantization_config = BitsAndBytesConfig(
+load_in_8bit=True)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", quantization_config=quantization_config)
+
+model_kwargs = {
+    "temperature": 0.0,
+}
+
+# We will stop generation as soon as the model outputs the end of Markdown to make inference faster
+stop_token_id = [tokenizer.convert_tokens_to_ids("---"), tokenizer.convert_tokens_to_ids("```")]
+llm = HuggingFaceLLM(model=model, tokenizer=tokenizer, max_new_tokens=1024,
+                     stopping_ids=stop_token_id, model_kwargs=model_kwargs)
+
+prompt_template = GEMMA_PROMPT
+cleaning_function = lambda code: code.split("```")[0]
 commandCenter = CommandCenter(
     ActionEngine(
-        DefaultLocalLLM(quantization_config=quantization_config),
+        llm,
         DefaultEmbedder(),
         GEMMA_PROMPT,
+        cleaning_function=cleaning_function,
     ),
     chromePath=f"{homedir}/chrome-linux64/chrome",
     chromedriverPath=f"{homedir}/chromedriver-linux64/chromedriver",
 )
-
 commandCenter.run(
     "https://huggingface.co",
     [
