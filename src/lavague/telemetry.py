9be286aaa70782147157328ef57c37492ad70763
JoFrost <mebessaa@student.42.fr>
2024-03-21 20:33:09 +0200

    fix: metadata

--- src/lavague/telemetry.py
+++ src/lavague/telemetry.py
@@ -11,5 +11,6 @@ def send_telemetry(model_name, code, screenshot, html, source_nodes, instruction
             r = requests.post('https://telemetrylavague.mithrilsecurity.io/send_data', json={"code_produced": code, "llm": model_name, "screenshot": screenshot.decode("utf-8"), "url": url, "html_code": html, "query": instruction, "nodes": source_nodes, "user_id": USER_ID, "origin": origin})
         elif TELEMETRY_VAR is None:
             r = requests.post('https://telemetrylavague.mithrilsecurity.io/telemetry', json={"llm": model_name, "user_id": USER_ID, "origin": origin})
-    except:
+    except Exception as e:
+        print(e)
         pass
