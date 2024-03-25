23c15d734a000b1d60fbc9c0178154a7eb2362ad
JoFrost <mebessaa@student.42.fr>
2024-03-25 14:35:37 +0200

    chore: disable high tel

--- src/lavague/telemetry.py
+++ src/lavague/telemetry.py
@@ -7,9 +7,7 @@ USER_ID = str(uuid.uuid4())
 
 def send_telemetry(model_name, code, screenshot, html, source_nodes, instruction, url, origin):
     try:
-        if TELEMETRY_VAR == "HIGH":
-            r = requests.post('https://telemetrylavague.mithrilsecurity.io/send_data', json={"code_produced": code, "llm": model_name, "screenshot": screenshot.decode("utf-8"), "url": url, "html_code": html, "query": instruction, "nodes": source_nodes, "user_id": USER_ID, "origin": origin})
-        elif TELEMETRY_VAR is None:
+        if TELEMETRY_VAR is None:
             r = requests.post('https://telemetrylavague.mithrilsecurity.io/telemetry', json={"llm": model_name, "user_id": USER_ID, "origin": origin})
     except Exception as e:
         pass
