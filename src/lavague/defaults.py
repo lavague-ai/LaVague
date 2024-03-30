d727cdc07f09e8b3782312577020ff1416a5016e
shubhamofbce <sraj13169@gmail.com>
2024-03-30 21:30:55 +0530

    Update exception msg

--- src/lavague/defaults.py
+++ src/lavague/defaults.py
@@ -51,7 +51,7 @@ async def get_playwright_driver():
     try:
         from playwright.async_api import async_playwright
     except (ImportError, ModuleNotFoundError) as error:
-        raise ImportError("Please install playwright using `pip install pytest-playwright` and then `playwright install` to install the necessary browser drivers") from error
+        raise ImportError("Please install playwright using `pip install playwright` and then `playwright install` to install the necessary browser drivers") from error
     p = await async_playwright().__aenter__()
     browser = await p.chromium.launch()
     page = await browser.new_page()
