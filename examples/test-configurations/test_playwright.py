8ce59c7f33ef4c1d5cae9c03c774038034b79172
Matthias BRUNEL <mbrunel@student.42.fr>
2024-04-18 20:13:22 +0000

    fix workflow

--- examples/test-configurations/test_playwright.py
+++ examples/test-configurations/test_playwright.py
@@ -5,7 +5,7 @@ from lavague.prompts import PLAYWRIGHT_PROMPT
 from lavague.defaults import (
     DefaultEmbedder,
     default_python_code_extractor,
-    default_get_selenium_driver,
+    default_get_playwright_driver,
 )
 
 
@@ -22,7 +22,7 @@ prompt_template = PLAYWRIGHT_PROMPT
 embedder = DefaultEmbedder()
 
 # default driver
-get_driver = default_get_selenium_driver
+get_driver = default_get_playwright_driver
 
 # Random test cleaning function - won't be used
 cleaning_function = default_python_code_extractor
