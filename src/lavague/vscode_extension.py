7956709d1f88e6be13c4b92974834d3c2287bfc3
JoFrost <mebessaa@student.42.fr>
2024-04-18 06:26:31 +0300

    chore: simplification of streaming

--- src/lavague/vscode_extension.py
+++ src/lavague/vscode_extension.py
@@ -14,18 +14,8 @@ try:
             engine_vscode = Config.make_default_action_engine()
         html: str = driver.page_source
         code_full = ""
-        for code in engine_vscode.get_action_streaming(line, html):
-            try:
-                code_full += code
-                r = requests.post("http://127.0.0.1:16500/push", json={"data": code})
-            except:
-                pass
         try:
-            code_full = engine_vscode.cleaning_function(code_full)
-            r = requests.post(
-                "http://127.0.0.1:16500/push",
-                json={"full_code": code_full, "over": True},
-            )
+            r = requests.post("http://127.0.0.1:16500/push", data=engine_vscode.get_action_streaming(line, html), stream=True)
         except:
             pass
 except:
