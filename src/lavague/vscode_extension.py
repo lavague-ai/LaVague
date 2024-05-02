be29082814eb9ce0d21fb153ab149321abe6ccb7
JoFrost <mebessaa@student.42.fr>
2024-05-02 12:35:04 +0000

    chore: add vscode ext

--- src/lavague/vscode_extension.py
+++ src/lavague/vscode_extension.py
@@ -1,9 +1,9 @@
+from typing import Optional
 from IPython.core.magic import register_line_magic
 import requests
 from .cli.config import Config
 
 try:
-
     @register_line_magic
     def lavague_exec(line: str):
         global engine_vscode
@@ -12,13 +12,22 @@ try:
         if "engine" not in globals():
             engine_vscode = Config.make_default_action_engine()
         html: str = driver.page_source
-        code_full = ""
         try:
             r = requests.post(
                 "http://127.0.0.1:16500/push",
-                data=engine_vscode.get_action_streaming_vscode(line, html, driver.getUrl()),
+                data=engine_vscode.get_action_streaming_vscode(line, html, driver.current_url),
                 stream=True,
             )
+        except Exception as e:
+            pass
+except:
+    pass
+
+try:
+    @register_line_magic
+    def lavague_export(line: Optional[str] = None):
+        try:
+            r = requests.post("http://127.0.0.1:16500/export")
         except:
             pass
 except:
