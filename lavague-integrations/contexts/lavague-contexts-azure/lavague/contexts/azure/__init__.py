c7dcdbf39da80cbae8bdc970fda33e6fe7dda70c
lyie28 <laurayie93@gmail.com>
2024-05-07 20:02:41 +0200

    integrations added

---
+++ lavague-integrations/contexts/lavague-contexts-azure/lavague/contexts/azure/__init__.py
@@ -0,0 +1,1 @@
+from lavague.contexts.azure.base import LiteLLMContext
---
+++ lavague-integrations/contexts/lavague-contexts-groq/lavague/contexts/groq/base.py
@@ -0,0 +1,20 @@
+from llama_index.llms.groq import Groq
+from llama_index.core.base.llms.base import BaseLLM
+from llama_index.core.base.embeddings.base import BaseEmbedding
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core import PromptTemplate
+from lavague.core import BaseHtmlRetriever, OpsmSplitRetriever
+from lavague.core import DEFAULT_PROMPT_TEMPLATE
+from lavague.core import BaseExtractor, PythonFromMarkdownExtractor
+from lavague.core import ActionContext
+from lavague.core.action_context import DEFAULT_MAX_TOKENS, DEFAULT_TEMPERATURE
+
+class GroqContext:
+    def from_defaults(
+        llm: BaseLLM = Groq(model="llama3-70b-8192", max_tokens=DEFAULT_MAX_TOKENS, temperature=DEFAULT_TEMPERATURE),
+        embedding: BaseEmbedding = OpenAIEmbedding(model="text-embedding-3-large"),
+        retriever: BaseHtmlRetriever = OpsmSplitRetriever(),
+        prompt_template: PromptTemplate = DEFAULT_PROMPT_TEMPLATE,
+        extractor: BaseExtractor = PythonFromMarkdownExtractor(),
+    ) -> ActionContext:
+        return ActionContext(llm, embedding, retriever, prompt_template, extractor)
