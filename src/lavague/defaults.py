62da820f03b48971b279e02f75bcedb851149478
JoFrost <mebessaa@student.42.fr>
2024-05-13 08:26:01 +0000

    feat: selenium by default & now auto download the drivers

--- src/lavague/defaults.py
+++ src/lavague/defaults.py
@@ -57,16 +57,11 @@ def default_python_code_extractor(markdown_text: str) -> Optional[str]:
 if SELENIUM_IMPORT:
 
     def default_get_selenium_driver() -> SeleniumDriver:
-        try:
-            from selenium import webdriver
-            from selenium.webdriver.chrome.service import Service
-            from selenium.webdriver.common.by import By
-            from selenium.webdriver.chrome.options import Options
-            from selenium.webdriver.common.keys import Keys
-        except (ImportError, ModuleNotFoundError) as error:
-            raise ImportError(
-                "Please install selenium using `pip install selenium`"
-            ) from error
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.common.keys import Keys
         import os.path
 
         chrome_options = Options()
@@ -74,42 +69,16 @@ if SELENIUM_IMPORT:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--window-size=1600,900")
 
-        homedir = os.path.expanduser("~")
-
-        # Paths to the chromedriver files
-        path_linux = f"{homedir}/chromedriver-linux64/chromedriver"
-        path_testing = f"{homedir}/chromedriver-testing/chromedriver"
-        path_mac = (
-            "Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing"
-        )
-
-        # To avoid breaking change kept legacy linux64 path
-        if os.path.exists(path_linux):
-            chrome_options.binary_location = f"{homedir}/chrome-linux64/chrome"
-            webdriver_service = Service(f"{homedir}/chromedriver-linux64/chromedriver")
-        elif os.path.exists(path_testing):
-            if os.path.exists(f"{homedir}/chrome-testing/{path_mac}"):
-                chrome_options.binary_location = f"{homedir}/chrome-testing/{path_mac}"
-            # Can add support here for other chrome binaries with else if statements
-            webdriver_service = Service(f"{homedir}/chromedriver-testing/chromedriver")
-        else:
-            raise FileNotFoundError("Neither chromedriver file exists.")
-
-        driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         return SeleniumDriver(driver)
     
     def evaluation_get_selenium_driver() -> SeleniumDriver:
         """Extra options to make the driver more static for evaluation purposes."""
-        try:
-            from selenium import webdriver
-            from selenium.webdriver.chrome.service import Service
-            from selenium.webdriver.common.by import By
-            from selenium.webdriver.chrome.options import Options
-            from selenium.webdriver.common.keys import Keys
-        except (ImportError, ModuleNotFoundError) as error:
-            raise ImportError(
-                "Please install selenium using `pip install selenium`"
-            ) from error
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.common.keys import Keys
         import os.path
 
         chrome_options = Options()
@@ -121,28 +90,7 @@ if SELENIUM_IMPORT:
         })
         chrome_options.add_argument('--proxy-server=127.0.0.1:9999')
 
-        homedir = os.path.expanduser("~")
-
-        # Paths to the chromedriver files
-        path_linux = f"{homedir}/chromedriver-linux64/chromedriver"
-        path_testing = f"{homedir}/chromedriver-testing/chromedriver"
-        path_mac = (
-            "Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing"
-        )
-
-        # To avoid breaking change kept legacy linux64 path
-        if os.path.exists(path_linux):
-            chrome_options.binary_location = f"{homedir}/chrome-linux64/chrome"
-            webdriver_service = Service(f"{homedir}/chromedriver-linux64/chromedriver")
-        elif os.path.exists(path_testing):
-            if os.path.exists(f"{homedir}/chrome-testing/{path_mac}"):
-                chrome_options.binary_location = f"{homedir}/chrome-testing/{path_mac}"
-            # Can add support here for other chrome binaries with else if statements
-            webdriver_service = Service(f"{homedir}/chromedriver-testing/chromedriver")
-        else:
-            raise FileNotFoundError("Neither chromedriver file exists.")
-
-        driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         return SeleniumDriver(driver)
 
 
