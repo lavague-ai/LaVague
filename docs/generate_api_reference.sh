93e557ffeb1708df59ccd6e048a80e5250cfd66a
Matthias BRUNEL <mbrunel@student.42.fr>
2024-05-22 01:28:22 +0200

    fix doc gen

--- docs/generate_api_reference.sh
+++ docs/generate_api_reference.sh
@@ -4,5 +4,8 @@ set -e  # Exit if any command fails
 # Install pdoc3 for generating Python API documentation
 pip install lavague pdoc3
 
+# set openai_api_key so that importing doesn't throw
+export OPENAI_API_KEY=""
+
 # Generate HTML documentation
 pdoc --html --skip-errors --template-dir docs/pdoc_template -o docs/docs/ lavague-core/lavague/core --force
