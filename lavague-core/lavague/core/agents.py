6f55254299f94e2f2146d6ef8b2101d061ad80e0
jashuajoy <tallajayakumar@gmail.com>
2024-06-13 11:48:10 +0530

    Added node display methods for logging(#332).
    fix: Added null checks and key checks for Dataframe

--- lavague-core/lavague/core/agents.py
+++ lavague-core/lavague/core/agents.py
@@ -15,6 +15,7 @@ from lavague.core.base_driver import BaseDriver
 from lavague.core.base_engine import ActionResult
 from lavague.core.utilities.telemetry import send_telemetry
 from PIL import Image
+from IPython.display import display, HTML, Code
 
 logging_print = logging.getLogger(__name__)
 logging_print.setLevel(logging.INFO)
@@ -316,3 +317,50 @@ class WebAgent:
             obs = self.driver.get_obs()
         send_telemetry(self.logger.return_pandas())
         return self.result
+
+    def display_previous_nodes(self, steps: int) -> None:
+        """prints out all nodes per each sub-instruction for given steps"""
+        dflogs = self.logger.return_pandas()
+        #check if dflogs are not null and not empty and engine_log is present in dflogs columns
+        if(dflogs is not None and dflogs.empty is False and "engine_log" in dflogs.columns):
+            if steps > len(dflogs):
+                print(f"Previous steps: {len(dflogs)}\nrequested steps: {steps}\nshowing available steps")
+            steps = len(dflogs) if steps > len(dflogs) else steps
+            for step in range(steps):
+                print(f"Step: {step}")
+                sub_ins = 0
+                if isinstance(dflogs.at[step, 'engine_log'], list):
+                    for subinst in dflogs.at[step, 'engine_log']:
+                        print(f"Sub-Instruction: {sub_ins}")
+                        sub_ins += 1
+                        x = 0
+                        for node in subinst['retrieved_html']:
+                            print(f"Node {x}")
+                            x = x + 1
+                            display(HTML(node)) # Display node as visual element
+                            display(Code(node, language="html")) # Display code
+        else:
+            print(f"No previous nodes available. Please run the agent atleast once to view previous steps")
+
+    def display_all_nodes(self) -> None:
+        """prints out all nodes per each sub-instruction"""
+        dflogs = self.logger.return_pandas()
+        #check if dflogs are not null and not empty and engine_log is present in dflogs columns
+        if(dflogs is not None and dflogs.empty is False and "engine_log" in dflogs.columns):
+            print(f"Number of steps: {len(dflogs)}")
+            steps = len(dflogs)
+            for step in range(steps):
+                print(f"Step: {step}")
+                sub_ins = 0
+                if isinstance(dflogs.at[step, 'engine_log'], list):
+                    for subinst in dflogs.at[step, 'engine_log']:
+                        print(f"Sub-Instruction: {sub_ins}")
+                        sub_ins += 1
+                        x = 0
+                        for node in subinst['retrieved_html']:
+                            print(f"Node: {x}")
+                            x = x + 1
+                            display(HTML(node)) # Display node as visual element
+                            display(Code(node, language="html")) # Display code
+        else:
+            print(f"No previous nodes available. Please run the agent atleast once to view previous steps")
