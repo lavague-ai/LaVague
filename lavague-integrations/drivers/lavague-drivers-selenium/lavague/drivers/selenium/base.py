c9536f63968ea918e17f542588f4e35d3168e984
JoFrost <mebessaa@student.42.fr>
2024-06-20 19:30:09 +0300

    chore: ruff

--- lavague-integrations/drivers/lavague-drivers-selenium/lavague/drivers/selenium/base.py
+++ lavague-integrations/drivers/lavague-drivers-selenium/lavague/drivers/selenium/base.py
@@ -200,15 +200,11 @@ driver.set_window_size({width}, {height} + height_difference)
         return self.driver.execute_script(js_code, *args)
 
     def scroll_up(self):
-        code = self.code_for_execute_script(
-            "window.scrollBy(0, -window.innerHeight);"
-        )
+        code = self.code_for_execute_script("window.scrollBy(0, -window.innerHeight);")
         self.exec_code(code)
 
     def scroll_down(self):
-        code = self.code_for_execute_script(
-            "window.scrollBy(0, window.innerHeight);"
-        )
+        code = self.code_for_execute_script("window.scrollBy(0, window.innerHeight);")
         self.exec_code(code)
 
     def code_for_execute_script(self, js_code: str, *args) -> str:
