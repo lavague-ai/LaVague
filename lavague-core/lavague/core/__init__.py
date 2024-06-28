9e57c56f2c4034ae24e0a99f81b9e4da42b5414f
JoFrost <mebessaa@student.42.fr>
2024-06-28 06:53:45 +0300

    chore: remove exec warning

--- lavague-core/lavague/core/__init__.py
+++ lavague-core/lavague/core/__init__.py
@@ -17,14 +17,7 @@ def telemetry_warning():
         warning_message = "\033[93mTelemetry is turned on. To turn off telemetry, set your LAVAGUE_TELEMETRY to 'NONE'\033[0m"
         warnings.warn(warning_message, UserWarning)
 
-
-def exec_warning():
-    warning_message = "\033[93mSecurity warning: This package executes LLM-generated code. Consider using this package in a sandboxed environment.\033[0m"
-    warnings.warn(warning_message, UserWarning)
-
-
 telemetry_warning()
-exec_warning()
 try:
     check_latest_version()
 except:
