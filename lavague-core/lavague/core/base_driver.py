fa7bd7805ad9cc89bc918103febb621c3e020451
JoFrost <mebessaa@student.42.fr>
2024-07-19 12:43:20 +0300

    chore: remove prints and last merge issues

--- lavague-core/lavague/core/base_driver.py
+++ lavague-core/lavague/core/base_driver.py
@@ -108,22 +108,6 @@ class BaseDriver(ABC):
         """
         pass
 
-    @abstractmethod
-    def get_tabs(self) -> str:
-        """Return description of the tabs opened with the current tab being focused.
-
-        Example of output:
-        Tabs opened:
-        0 - Overview - OpenAI API
-        1 - [CURRENT] Nos destinations Train - SNCF Connect
-        """
-        pass
-
-    @abstractmethod
-    def switch_tab(self, tab_id: int) -> None:
-        """Switch to the tab with the given id"""
-        pass
-
     def get_tabs(self) -> str:
         """Return description of the tabs opened with the current tab being focused.
 
