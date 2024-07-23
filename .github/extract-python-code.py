70c5f89383f9b1628c1198e429c5aa37ece02efc
lyie28 <laurayie93@gmail.com>
2024-07-23 14:51:31 +0200

    fmt

--- .github/extract-python-code.py
+++ .github/extract-python-code.py
@@ -16,10 +16,10 @@ def extract_python_code(file_path):
     modified_code = extracted_code.replace("agent.demo", "agent.run")
 
     # Remove occurrences of `, display=True`
-    modified_code = re.sub(r',\s*display=True', '', modified_code)
+    modified_code = re.sub(r",\s*display=True", "", modified_code)
 
     # Remove occurrences of `, display=True`
-    modified_code = re.sub(r'\bdisplay\([^\)]*\)', '', modified_code)
+    modified_code = re.sub(r"\bdisplay\([^\)]*\)", "", modified_code)
     return modified_code
 
 
