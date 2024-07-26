c5048fb65023622897d16985c2706776ffebe2ee
lyie28 <laurayie93@gmail.com>
2024-07-26 15:17:57 +0200

    anthropic update and fmt

--- lavague-tests/contexts/fireworks_context.py
+++ lavague-tests/contexts/fireworks_context.py
@@ -7,5 +7,6 @@ token_counter = TokenCounter()
 
 # init context
 fireworks_context = FireworksContext()
-context = Context(fireworks_context.llm, fireworks_context.mm_llm, fireworks_context.embedding)
-
+context = Context(
+    fireworks_context.llm, fireworks_context.mm_llm, fireworks_context.embedding
+)
