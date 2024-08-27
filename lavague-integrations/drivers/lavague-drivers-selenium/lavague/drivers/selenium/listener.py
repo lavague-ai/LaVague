9df7f7b8aa415fbdc3fa2a7ac7281e18baf31d36
Alexis Deprez <deprez.alexis@laposte.net>
2024-08-27 17:10:21 +0200

    feat: ignore readonly and disabled aria attributes
    Many websites misuse these accessibility attributes ; therefore it's safer to ignore them

--- lavague-integrations/drivers/lavague-drivers-selenium/lavague/drivers/selenium/listener.py
+++ lavague-integrations/drivers/lavague-drivers-selenium/lavague/drivers/selenium/listener.py
@@ -5,5 +5,7 @@ from lavague.core.listener import EventListener as BaseEventListener
 class EventListener(BaseEventListener):
     def __init__(self, driver: WebDriver):
         super().__init__(
-            lambda script, xpaths: driver.execute_async_script(script, xpaths)
+            lambda script, prevent_action, xpaths: driver.execute_async_script(
+                script, prevent_action, xpaths
+            )
         )
